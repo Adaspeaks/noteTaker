8ed6dab0668298a5583193d21593a160dd30d74a
Adaspeaks <adaspeaks@gmail.com>
2021-07-31 17:34:42 -0700

    get route for home

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const fs = require("fs/promises");
 const express = require("express");
+const routes = require("")
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -8,6 +10,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/public/index.html");
+  });
 
 app.listen(PORT, () => 
     console.log((`App listening at http://localhost:${PORT}`))
