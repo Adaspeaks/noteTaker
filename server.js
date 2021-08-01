877856299cbc17a8c8830c1502f3b57694007b60
Adaspeaks <adaspeaks@gmail.com>
2021-07-31 18:01:32 -0700

    added post routes for notes

--- server.js
+++ server.js
@@ -19,22 +19,29 @@ app.get("/notes", function (req, res) {
   res.sendFile(__dirname + "/public/notes.html");
 });
 
-app.get("/api/notes", async function (req, res){
-    try {
-        const data = await fs.readFile("./db/db.json", "utf8");
-        res.json(JSON.parse(data));
-    } catch (err) {
-        res.status(500).end("failed to reach /api/notes")
-    }
+app.get("/api/notes", async function (req, res) {
+  try {
+    const data = await fs.readFile("./db/db.json", "utf8");
+    res.json(JSON.parse(data));
+  } catch (err) {
+    res.status(500).end("failed to reach /api/notes");
+  }
 });
 
-app.post("/api/notes", async function (req, res){
-    try {
-        
-    } catch (err) {
-        
-    }
-})
+app.post("/api/notes", async function (req, res) {
+  try {
+    const note = req.body;
+    const data = await fs.readFile("./db/db.json", "utf8");
+    const parsedData = JSON.parse(data);
+    note.id = uuid.v1();
+    parsedData.push(note);
+    await fs.writeFile("db/db.json", JSON.stringify(parsedData));
+
+    res.json(note);
+  } catch (err) {
+    res.status(500).end("failed to post note to /api/notes");
+  }
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
